8ce91bcde238b4ac0be46899acc2e3bb1395a420
Ace Lite <47698279+Ace-Lite@users.noreply.github.com>
2024-06-03 19:25:06 +0200

    New starman effects

--- PKZ_Current/Lua/entities/items/item_starman.lua
+++ PKZ_Current/Lua/entities/items/item_starman.lua
@@ -8,6 +8,41 @@ local StarSettings = {
 	[2] = function(a) a.scale = a.spawnpoint.scale*3>>1 end;
 }
 
+local T_InvColors = {
+	--SKINCOLOR_CYAN;
+	--SKINCOLOR_COBALT;
+	--SKINCOLOR_PURPLE;
+	--SKINCOLOR_RED;
+	--SKINCOLOR_ORANGE;
+	--SKINCOLOR_GREEN;
+	--SKINCOLOR_MINT;
+	--SKINCOLOR_AETHER;
+	SKINCOLOR_MARIOINVULN1,
+	SKINCOLOR_MARIOINVULN2,
+	SKINCOLOR_MARIOINVULN3,
+	SKINCOLOR_MARIOINVULN4,
+	SKINCOLOR_MARIOINVULN5,
+	SKINCOLOR_MARIOINVULN6,
+	SKINCOLOR_MARIOINVULN7,
+	SKINCOLOR_MARIOINVULN8,
+}
+
+local translationanim = {
+	nil,
+	nil,
+	"STARMANTRANSLATION1",
+	"STARMANTRANSLATION2",
+	"STARMANTRANSLATION3",
+	"STARMANTRANSLATION4",
+	"STARMANTRANSLATION5",
+	"STARMANTRANSLATION5",
+	"STARMANTRANSLATION4",
+	"STARMANTRANSLATION3",
+	"STARMANTRANSLATION2",
+	"STARMANTRANSLATION1",
+}
+
+
 addHook("MobjThinker", function(a)
 	//Behavioral setting
 	if a.behsetting == nil then
@@ -35,16 +70,32 @@ addHook("MobjThinker", function(a)
 		a.flags = $ &~ MF_NOGRAVITY
 	end
 
-	if (8 & leveltime) >> 2
-		local poweruppar = P_SpawnMobjFromMobj(a, P_RandomRange(-16,16) << FRACBITS, P_RandomRange(-16,16) << FRACBITS, P_RandomRange(0,32) << FRACBITS, MT_POPPARTICLEMAR)
-		poweruppar.state = S_INVINCSTAR
+
+	if not (leveltime % 3) then
+		A_GhostMe(a)
+		-- follow invfog
+		local color = T_InvColors[((leveltime-1 >> 3) % #T_InvColors)+1]
+		local fog = P_SpawnMobjFromMobj(a, 0, 0, 0, MT_BLOCKVIS)
+		fog.state = S_WIILIKEINVFOG
+		fog.color = color
+		fog.frame = $| ((leveltime % 2) == 1 and FF_HORIZONTALFLIP or 0)
+		fog.fuse = 3
+
+		local poweruppar = P_SpawnMobjFromMobj(a, 0, 0, a.height/2, MT_POPPARTICLEMAR)
+		poweruppar.state = S_SM64SPARKLES
 		poweruppar.scale = a.scale
-		poweruppar.color = SKINCOLOR_AETHER
+		poweruppar.color = color
 		poweruppar.fuse = TICRATE
 	end
 
-	A_GhostMe(a)
-	if P_IsObjectOnGround(a)
+	local translation = translationanim[(leveltime % #translationanim)+1]
+	if translation then
+		a.translation = translation
+	else
+		a.translation = nil
+	end
+
+	if P_IsObjectOnGround(a) then
 		a.z = $ + P_MobjFlip(a)
 		P_SetObjectMomZ(a, 12 << FRACBITS, false)
 	end
