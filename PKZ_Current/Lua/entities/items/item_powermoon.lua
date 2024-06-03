ec5c6c8017623c721b85f4398654231334d12188
Ace Lite <47698279+Ace-Lite@users.noreply.github.com>
2024-06-03 19:24:46 +0200

    New starmoon effects

--- PKZ_Current/Lua/entities/items/item_powermoon.lua
+++ PKZ_Current/Lua/entities/items/item_powermoon.lua
@@ -11,24 +11,86 @@ addHook("PlayerSpawn", function(player)
 	end
 end)
 
+local function P_SpawnSpecialBGStar(a, tic, col, momz, fuse, momx, momy, scale, color)
+	if (leveltime % 8)/tic then
+		local randomness_jitter = P_RandomRange(-4, 4) << FRACBITS
+		local x = P_RandomRange(-24,24) << FRACBITS + randomness_jitter
+		local y = P_RandomRange(-24,24) << FRACBITS + randomness_jitter
+		local z = P_RandomRange(0,48) << FRACBITS + randomness_jitter
+		local poweruppar
+
+		if P_RandomKey(16) == 2 then
+			poweruppar = P_SpawnMobjFromMobj(a, x, y, z, MT_POPPARTICLEMAR)
+			poweruppar.state = S_SM64BGSTAR
+			poweruppar.dispoffset = -3
+			poweruppar.angle = a.angle
+			poweruppar.scale = scale or a.scale
+			poweruppar.color = color or a.color
+			poweruppar.colorized = col
+			poweruppar.momz = momz
+			poweruppar.momx = (momx or 1)/2
+			poweruppar.momy = (momy or 1)/2
+			poweruppar.fuse = fuse
+		end
+
+		poweruppar = P_SpawnMobjFromMobj(a, x, y, z, MT_POPPARTICLEMAR)
+		poweruppar.state = S_SM64SPARKLESSINGLE
+		poweruppar.angle = a.angle
+		poweruppar.scale = scale or a.scale
+		poweruppar.color = color or a.color
+		poweruppar.colorized = col
+		poweruppar.momz = momz
+		poweruppar.momx = (momx or 1)/2
+		poweruppar.momy = (momy or 1)/2
+		poweruppar.fuse = fuse
+	end
+end
+
+-- PUP2A0
+-- PUP3A0
+
+local splitunit = FRACUNIT/32
+local beamscale = 4*FRACUNIT/3
+
 addHook("MobjThinker", function(mo)
 	mo.angle = $ + ANG1*3
-	if (8 & leveltime)/6 then
-		local poweruppar = P_SpawnMobjFromMobj(mo,
-		P_RandomRange(-32,32) << FRACBITS,
-		P_RandomRange(-32,32) << FRACBITS,
-		P_RandomRange(0,64) << FRACBITS,
-		MT_POPPARTICLEMAR)
 
-		poweruppar.state = S_INVINCSTAR
-		poweruppar.scale = mo.scale
-		poweruppar.color = SKINCOLOR_GOLD
-		poweruppar.fuse = TICRATE
+	if not mo.rainbowcorona then
+		mo.rainbowcorona = P_SpawnMobjFromMobj(mo, 0, 0, 0, MT_OVERLAY)
+		mo.rainbowcorona.state = S_INVISIBLE
+		mo.rainbowcorona.sprite = SPR_MMPARTICLES
+		mo.rainbowcorona.frame = 17|FF_TRANS70|FF_ADD
+
+		mo.rainbowcorona.target = mo
+		mo.rainbowcorona.spriteyoffset = 18*FRACUNIT
+		mo.rainbowcorona.spritexscale = 5*FRACUNIT/3
+		mo.rainbowcorona.spriteyscale = mo.rainbowcorona.spritexscale
 	end
+
+	if not (leveltime % 6) then
+		local ray = P_SpawnMobjFromMobj(mo, 0, 0, 0, MT_POPPARTICLEMAR)
+		ray.state = S_INVISIBLE
+		ray.rayeffect = true
+		ray.target = mo
+		ray.angle = FixedAngle(P_RandomRange(1, 360) << FRACBITS)
+		ray.rollangle = FixedAngle(P_RandomRange(1, 360) << FRACBITS)
+		ray.scale = beamscale+P_RandomRange(-32, 16)*splitunit
+		ray.fuse = P_RandomRange(1*TICRATE, 2*TICRATE)
+	end
+
+	--if not mo.corona then
+	--	mo.corona = P_SpawnMobjFromMobj(mo, 0, 0, 0, MT_OVERLAY)
+	--	mo.corona.state = S_INVISIBLE
+	--	mo.corona.sprite = SPR_PUP2
+	--	mo.corona.frame = A|FF_TRANS60|FF_ADD
+	--end
+
+	P_SpawnSpecialBGStar(mo, 6, false, 0, TICRATE, 0, 0, FRACUNIT, SKINCOLOR_GOLD)
 end, MT_MARPOWERMOON)
 
 addHook("MobjRemoved", function(mo)
 	PowerMoonNum = $-1
+	S_StartSound(nil, sfx_maodd3)
 	if PowerMoonNum <= 0 then
 		G_ExitLevel()
 	end
