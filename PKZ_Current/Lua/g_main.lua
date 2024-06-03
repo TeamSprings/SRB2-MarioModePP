a4b2a6969811d4cff408c74e286437653855d494
Ace Lite <47698279+Ace-Lite@users.noreply.github.com>
2024-06-03 19:26:23 +0200

    misc stuff

--- PKZ_Current/Lua/g_main.lua
+++ PKZ_Current/Lua/g_main.lua
@@ -1,4 +1,4 @@
-/* 
+/*
 		Pipe Kingdom Zone's Main - g_main.lua
 
 Description:
@@ -11,21 +11,21 @@ Contributors: Skydusk
 local PKZ_Table = {
 
 	-- Core
-	active = true, 
+	active = true,
 	version = "2.0", // current
 	versnum = 199, // Change to 200 on release
 	betarelease = "0.78.22112023",
-	
+
 	// Mario Mode Plus
 	game_type = "Pipe_Kindom_Zone",
 
 	-- Collectibles
 	dragonCoins = 0,
-	maxDrgCoins = 39, //There are only 30 in levels, other 10 are gain through time/coin required means	
+	maxDrgCoins = 39, //There are only 30 in levels, other 10 are gain through time/coin required means
 	dragonCoinTags = {},
 	ringsCoins = 0,
 	roomHubKey = 0,
-	
+
 	unlocks_flags = {
 		["KEY"] = 1,
 	},
@@ -36,7 +36,7 @@ local PKZ_Table = {
 	levellist = {
 		[31] = {reqVisit = false; recordedtime = 0; timeattack = 1000; timeattackDGid = 40; coins = {}, new_coin = 2},
 		[34] = {reqVisit = false; recordedtime = 0; timeattack = 5250; timeattackDGid = 39; coins = {1, 2, 3, 4, 5, 29}, new_coin = 1},
-		[46] = {reqVisit = false; recordedtime = 0; timeattack = 1000; timeattackDGid = 41; coins = {}, new_coin = 2},		
+		[46] = {reqVisit = false; recordedtime = 0; timeattack = 1000; timeattackDGid = 41; coins = {}, new_coin = 2},
 		[35] = {reqVisit = true; recordedtime = 0; timeattack = 1000; timeattackDGid = 38; coins = {}, new_coin = 1},
 		[36] = {reqVisit = true; recordedtime = 0; timeattack = 1000; timeattackDGid = 37; coins = {}, new_coin = 1},
 		[37] = {reqVisit = true; recordedtime = 0; timeattack = 1000; timeattackDGid = 36; coins = {}, new_coin = 0},
@@ -51,101 +51,101 @@ local PKZ_Table = {
 
 	-- Hub Return
 	hub_warp = 1,
-	
+
 	-- Mainly for all radars and misc. purposes
 	curlvl = {
 		mobj_scoins = {},
 		mobj_smoons = {},
 	},
-	
+
 	-- Achivement shall get refactored into savefile saving, this is performance wasting
 	checklist = {
-		{name = "Beat Bowser", reward = "Key to Extra LVLs", 
+		{name = "Beat Bowser", reward = "Key to Extra LVLs",
 		toggle = function(table)
-			local save_data = table.getSaveData()	
-			if save_data.unlocked & table.unlocks_flags["KEY"] then 
-				return true 
-			else 
-				return false 
-			end
-		end;};
-		
-		{name = "Beat All Goomba Races", reward = "L Statue.", 
-		toggle = function(table) 
-			return false 
-		end;};
-		
-		{name = "Get All Pipe Coins", reward = "Level Select", 
-		toggle = function(table) 
-			return false 
-		end;};		
-		
-		{name = "Get All Dragon Coins", reward = "Cheats", 
-		toggle = function(table)
-			local coins_data = table.getSaveData().coins
-			if #coins_data == table.maxDrgCoins then 
-				return true 
-			else 
+			local save_data = table.getSaveData()
+			if save_data.unlocked & table.unlocks_flags["KEY"] then
+				return true
+			else
 				return false
 			end
-		end;};		
-		
-		{name = "Get All Achivements", reward = "Congratulation!", 
-		toggle = function(table) 
+		end;};
+
+		{name = "Beat All Goomba Races", reward = "L Statue.",
+		toggle = function(table)
 			return false
-		end;};		
+		end;};
+
+		{name = "Get All Pipe Coins", reward = "Level Select",
+		toggle = function(table)
+			return false
+		end;};
+
+		{name = "Get All Dragon Coins", reward = "Cheats",
+		toggle = function(table)
+			local coins_data = table.getSaveData().coins
+			if #coins_data == table.maxDrgCoins then
+				return true
+			else
+				return false
+			end
+		end;};
+
+		{name = "Get All Achivements", reward = "Congratulation!",
+		toggle = function(table)
+			return false
+		end;};
 	};
-	
+
 	EV_BOWSER 	= 1,
 	EV_ALLRACES = 2,
 	EV_ALLCOINS = 4,
-	
+
 	-- ALL ACHIVEMENTS
 	EV_ALLACHIVEMENTS = 1|2|4,
-	
+
 	-- REWARDS
 	RE_KEY = 1,
 	RE_STATUEL = 2,
 	RE_LEVELSELECT = 4,
 	RE_CHEATS = 8,
-	
+
 	achivement_definitions = {},
-	
+
 	-- MARIO MODE ++ GAME FLAGS
 	-- TO DO:
 	gameFlags = 0,
-	
-	GF_NOSONICHURT 	= 1, -- DISABLES THE CLASSIC SONIC RING DROP MECHANIC 
+
+	GF_NOSONICHURT 	= 1, -- DISABLES THE CLASSIC SONIC RING DROP MECHANIC
 	GF_HARDMODE 	= 2, -- CHEAT!
-	GF_EVIL 		= 4, -- CHEAT!	
+	GF_EVIL 		= 4, -- CHEAT!
 	GF_LISREAL 		= 8, -- CHEAT!
-	
+
 	-- code states
-	nosonicrings = false,	
+	nosonicrings = false,
 	hardMode = false,
 	evil = false,
 	lisreal = false,
-	
+
 		-- Just for compatibility purposes
 	hideHud = false,
 	replaceHud = true,
-	
-	/* 
+
+	/*
 	*	Either, they have different custom functionality
 	*	or... they are simply disabled, due to lack of compability
 	*	They are still playable tho!
-	*/ 
+	*/
 	disabledSkins = {
 	["mario"] = false,
-	["luigi"] = false,	
+	["luigi"] = false,
 	["modernsonic"] = false,
 	},
-		
-	-- MARIO MODE ++ SPECIFIC PLAYER FLAGS 
+
+	-- MARIO MODE ++ SPECIFIC PLAYER FLAGS
 	-- TO DO:
 	skinSpecifications = {
 	["mario"] = 1|8,
-	["luigi"] = 1|8,	
+	["luigi"] = 1|8,
 	["modernsonic"] = 0,
 	["default"] = 1|2|4|8,
 	},
@@ -162,7 +162,7 @@ local coin_types = {
 	["DRAGON_COINS"] = 0,
 	["PIPE_COINS"] = 1,
 	["A_COINS"] = 2,
-	["STAR_COINS"] = 3, 
+	["STAR_COINS"] = 3,
 }
 
 local instruction_set = {
@@ -170,7 +170,7 @@ local instruction_set = {
 		PKZ_Table.game_type = line[2]
 		return cmap, lvl_data, data
 	end,
-	["COIN_MILESTONES"] = function(cmap, lvl_data, data, line) 	
+	["COIN_MILESTONES"] = function(cmap, lvl_data, data, line)
 		for i = 2, #line do
 			data.max_specialcoins = $+1
 			local current_num = string.gsub(string.gsub(line[i], ", ", ""), ",","")
@@ -187,7 +187,7 @@ local instruction_set = {
 			recordedtime = 0,
 			timeattackDGid = nil,
 			reqVisit = false,
-		}		
+		}
 		table.insert(data.level_ids, cmap)
 		if not data.level_ids.default then
 			data.level_ids.default = 1
@@ -200,30 +200,30 @@ local instruction_set = {
 		if cmap then
 			PKZ_Table.hub_warp = cmap
 		end
-		return cmap, lvl_data, data		
-	end,	
-	["REQUIRED_VISIT"] = function(cmap, lvl_data, data, line) 
+		return cmap, lvl_data, data
+	end,
+	["REQUIRED_VISIT"] = function(cmap, lvl_data, data, line)
 		if lvl_data[cmap] then
 			local bool = string.upper(line[2] or "false")
 
 			lvl_data[cmap].reqVisit = (bool == "TRUE")
-			--print("Map "..cmap.." requirement of visit is set: "..line[2])		
+			--print("Map "..cmap.." requirement of visit is set: "..line[2])
 		end
 		return cmap, lvl_data, data
 	end,
 	-- Event <event_id> [<event_rew>] [description]
-	--["EVENT"] = function(cmap, lvl_data, data, line) 
+	--["EVENT"] = function(cmap, lvl_data, data, line)
 	--	if lvl_data[cmap] then
 	--		local bool = string.upper(line[2] or "false")
 	--
 	--		lvl_data[cmap].reqVisit = (bool == "TRUE")
-	--		--print("Map "..cmap.." requirement of visit is set: "..line[2])		
+	--		--print("Map "..cmap.." requirement of visit is set: "..line[2])
 	--	end
 	--	return cmap, lvl_data, data
-	--end,	
-	["TIME_ATTACK"] = function(cmap, lvl_data, data, line) 
+	--end,
+	["TIME_ATTACK"] = function(cmap, lvl_data, data, line)
 		if lvl_data[cmap] then
-	
+
 			lvl_data[cmap].timeattack = tonumber(line[2] or 0)
 			if line[3] and string.upper(line[3]) == "TRUE" then
 				data.max_specialcoins =	$+1
@@ -232,20 +232,20 @@ local instruction_set = {
 			end
 			--print("Map "..cmap.." timeattack requirement: "..lvl_data[cmap].timeattack)
 		end
-		return cmap, lvl_data, data		
+		return cmap, lvl_data, data
 	end,
-	["SPECIAL_COIN"] = function(cmap, lvl_data, data, line) 
+	["SPECIAL_COIN"] = function(cmap, lvl_data, data, line)
 		if lvl_data[cmap] then
-		
+
 			if line[3] then
 				for i = 1, tonumber(line[3]) do
 					table.insert(lvl_data[cmap].coins, data.max_specialcoins+i)
-				end	
-			
+				end
+
 				data.max_specialcoins =	$+tonumber(line[3])
 				lvl_data[cmap].new_coin = coin_types[string.upper(line[2])] or coin_types["A_COINS"]
 				lvl_data[cmap].num_coin = tonumber(line[3])
-				--print("Map "..cmap.." amount of special coins: "..tonumber(line[3]))				
+				--print("Map "..cmap.." amount of special coins: "..tonumber(line[3]))
 			end
 		end
 		return cmap, lvl_data, data
@@ -254,17 +254,17 @@ local instruction_set = {
 
 PKZ_Table.loadDefs = function()
 	print('[Mario Mode++]'.." Parsing Data")
-	
+
 	local cmap = 0
 	local def = TBSlib.parse(MM_setup)
 	local lvl_data = {}
 	local data = {
 		level_ids = {},
 		milestones = {},
-		events = {},		
+		events = {},
 		max_specialcoins = 0,
 	}
-	
+
 	for i = 1, #def do
 		local line = def[i]
 		if not (line and line[1]) then continue end
@@ -272,13 +272,13 @@ PKZ_Table.loadDefs = function()
 			cmap, lvl_data, data = instruction_set[string.upper(line[1])](cmap, lvl_data, data, line)
 		end
 	end
-	
+
 	PKZ_Table.achivement_definitions = data.events
 	PKZ_Table.maxDrgCoins = data.max_specialcoins
 	PKZ_Table.dragonCoinRingSelect = data.milestones
 	PKZ_Table.listoflevelIDs = data.level_ids
 	PKZ_Table.levellist = lvl_data
-	
+
 	print('[Mario Mode++]'.." Definitions loaded")
 end
 
@@ -313,14 +313,14 @@ addHook("MapLoad", do
 	ctrl_inputs.left[1], ctrl_inputs.left[2] = input.gameControlToKeyNum(GC_STRAFELEFT)
 	ctrl_inputs.right[1], ctrl_inputs.right[2] = input.gameControlToKeyNum(GC_STRAFERIGHT)
 	ctrl_inputs.turl[1], ctrl_inputs.turl[2] = input.gameControlToKeyNum(GC_TURNLEFT)
-	ctrl_inputs.turr[1], ctrl_inputs.turr[2] = input.gameControlToKeyNum(GC_TURNRIGHT)	
+	ctrl_inputs.turr[1], ctrl_inputs.turr[2] = input.gameControlToKeyNum(GC_TURNRIGHT)
 
 	ctrl_inputs.jmp[1], ctrl_inputs.jmp[2] = input.gameControlToKeyNum(GC_JUMP)
 	ctrl_inputs.spn[1], ctrl_inputs.spn[2] = input.gameControlToKeyNum(GC_SPIN)
 	ctrl_inputs.cb1[1], ctrl_inputs.cb1[2] = input.gameControlToKeyNum(GC_CUSTOM1)
     ctrl_inputs.cb2[1], ctrl_inputs.cb2[2] = input.gameControlToKeyNum(GC_CUSTOM2)
     ctrl_inputs.cb3[1], ctrl_inputs.cb3[2] = input.gameControlToKeyNum(GC_CUSTOM3)
-	ctrl_inputs.tfg[1], ctrl_inputs.tfg[2] = input.gameControlToKeyNum(GC_TOSSFLAG)		
+	ctrl_inputs.tfg[1], ctrl_inputs.tfg[2] = input.gameControlToKeyNum(GC_TOSSFLAG)
 
     ctrl_inputs.con[1], ctrl_inputs.con[2] = input.gameControlToKeyNum(GC_CONSOLE)
 end)
@@ -331,7 +331,7 @@ rawset(_G, "ctrl_inputs", ctrl_inputs)
 -- create mario mode player userdata
 addHook("PlayerSpawn", function(p)
 	p.tempmariomode = {}
-	if not p.mariomode then 
+	if not p.mariomode then
 		p.mariomode = {}
 	end
 	if p.mo and p.mo.valid and not p.mo.mariomode then
@@ -340,7 +340,7 @@ addHook("PlayerSpawn", function(p)
 end)
 
 -- create mario mode hud userdata
-if not hud.mariomode then	
+if not hud.mariomode then
 	hud.mariomode = {}
-	hud.mariomode.title_ticker = {} 
+	hud.mariomode.title_ticker = {}
 end
---
+++ PKZ_Current/Lua/game/modes/mode_hard.lua
@@ -0,0 +1,199 @@
+
+
+local hard_mode_flats = {
+	["PTZWCT3"] = "MAROWRGA",
+	["PTZWCT5"] = "MAROWRGB",
+	["MAROWRG1"] = "MAROWRGC",
+	["MAROWRG2"] = "MAROWRGD",
+	["MAROWRG3"] = "MAROWRGE",
+	["MAROWRG4"] = "MAROWRGF",
+	["MAROWRG5"] = "MAROWRGG",
+	["MAROWRG6"] = "MAROWRGH",
+	["MARGR1F"] = "MAHGR1F",
+	["MARGR2F"] = "MAHGR2F",
+	["MARGR3F"] = "MAHGR3F",
+	["MARGR4F"] = "MAHGR4F",
+	["MARGRA1"] = "MAHGRA1",
+	["MARGRA2"] = "MAHGRA2",
+	["MARGRAF"] = "MAHGRAF",
+	["MARGRAS"] = "MAHGRAS",
+	["MARG1F1"] = "MAHG1F1",
+	["MARG1F3"] = "MAHG1F3",
+	["MARG1F4"] = "MAHG1F4",
+	["MARG1F5"] = "MAHG1F5",
+	["MARG1F6"] = "MAHG1F6",
+	["MARG1F7"] = "MAHG1F7",
+	["MARG1F8"] = "MAHG1F8",
+	["MARG1F9"] = "MAHG1F9",
+	["MARG1FA"] = "MAHG1FA",
+	["MARG1FB"] = "MAHG1FB",
+	["MARG1FC"] = "MAHG1FC",
+	["MARG1FE"] = "MAHG1FE",
+	["MARG1FF"] = "MAHG1FF",
+	["MARG1FG"] = "MAHG1FG",
+}
+
+local hard_mode_textur = {}
+
+local hard_mode_replacements = {
+	[MT_MGREENKOOPA] = function(a)
+		a.color = P_RandomRange(0,1) and SKINCOLOR_RED or SKINCOLOR_SAPPHIRE
+		a.extravalue1 = 2
+		local head = P_SpawnMobjFromMobj(a, 0,0,0, MT_OVERLAY)
+		head.target = a
+		head.state = S_INVISIBLE
+		head.sprite = SPR_MKOP
+		head.frame = H
+		head.color = a.color
+	end,
+	[MT_GOOMBA] = function(a)
+		--local galoom = P_SpawnMobjFromMobj(a, 0,0,0, MT_GALOOMBA)
+		--galoom.scale = a.scale
+		--P_RemoveMobj(a)
+		a.scale = $+FRACUNIT
+		a.health = 2
+	end,
+	[MT_BLUEGOOMBA] = function(a)
+		local buzz = P_SpawnMobjFromMobj(a, 0,0,0, MT_BUZZYBEETLE)
+		buzz.scale = a.scale+FRACUNIT/4
+		P_RemoveMobj(a)
+	end,
+	--[MT_BLUEGOOMBA] = function(a)
+	--	P_SpawnMobjFromMobj(a, 0,0,0, MT_BUZZYBEETLE)
+	--	P_RemoveMobj(a)
+	--end,
+	[MT_MARIOBUSH1] = function(a) a.color = SKINCOLOR_RED end,
+	[MT_MARIOBUSH2] = function(a) a.color = SKINCOLOR_RED end,
+	[MT_MARIOBUSH] = function(a) a.color = SKINCOLOR_ORANGE end,
+	[MT_MAR64TREE] = function(a) a.color = P_RandomRange(0,1) and SKINCOLOR_ORANGE or SKINCOLOR_RED end,
+}
+
+
+--[[
+local cheats_transfer = {
+	["evil"] = function()
+		if netgame and consoleplayer ~= server then
+			CONS_Printf(consoleplayer, "\x85".."WARNING:".."\x80".."This cheat is only available to host.")
+			return
+		end
+		PKZ_Table.evil = true
+		PKZ_Table.cheatrecord = true
+
+		if not netgame then
+			G_SetCustomExitVars(gamemap, 1)
+			G_ExitLevel()
+			TBS_Menu:toggleMenu(false)
+		end
+
+		print("\x85".."NOTE:".."\x80".."Evil setting is on, prepare for cosequences. Requires restart of map.")
+	end,
+	["hard"] = function()
+		if netgame and consoleplayer ~= server then
+			CONS_Printf(consoleplayer, "\x85".."WARNING:".."\x80".."This cheat is only available to host.")
+			return
+		end
+		PKZ_Table.hardMode = true
+		if not netgame then
+			G_SetCustomExitVars(gamemap, 1)
+			G_ExitLevel()
+			TBS_Menu:toggleMenu(false)
+		end
+		print("\x85".."NOTE:".."\x80".."Hard difficulty is on. Requires restart of map.")
+	end,
+	["nosonicrings"] = function()
+		PKZ_Table.nosonicrings = PKZ_Table.nosonicrings ~= true and true or false
+	end
+}
+
+
+
+addHook("MapLoad", function()
+	if #hard_mode_textur == 0 then
+		hard_mode_textur = {
+		[R_TextureNumForName("PTZWCT3")] = R_TextureNumForName("MAROWRGA"),
+		[R_TextureNumForName("PTZWCT5")] = R_TextureNumForName("MAROWRGB"),
+		[R_TextureNumForName("MAROWRG1")] = R_TextureNumForName("MAROWRGC"),
+		[R_TextureNumForName("MAROWRG2")] = R_TextureNumForName("MAROWRGD"),
+		[R_TextureNumForName("MAROWRG3")] = R_TextureNumForName("MAROWRGE"),
+		[R_TextureNumForName("MAROWRG4")] = R_TextureNumForName("MAROWRGF"),
+		[R_TextureNumForName("MAROWRG5")] = R_TextureNumForName("MAROWRGG"),
+		[R_TextureNumForName("MAROWRG6")] = R_TextureNumForName("MAROWRGH"),
+		[R_TextureNumForName("MARGR1F")] = R_TextureNumForName("MAHGR1F"),
+		[R_TextureNumForName("MARGR2F")] = R_TextureNumForName("MAHGR2F"),
+		[R_TextureNumForName("MARGR3F")] = R_TextureNumForName("MAHGR3F"),
+		[R_TextureNumForName("MARGR4F")] = R_TextureNumForName("MAHGR4F"),
+		[R_TextureNumForName("MARGRA1")] = R_TextureNumForName("MAHGRA1"),
+		[R_TextureNumForName("MARGRA2")] = R_TextureNumForName("MAHGRA2"),
+		[R_TextureNumForName("MARGRAF")] = R_TextureNumForName("MAHGRAF"),
+		[R_TextureNumForName("MARGRAS")] = R_TextureNumForName("MAHGRAS"),
+		}
+	end
+
+	if PKZ_Table.evil then
+		local exchangetable = {}
+
+		for mobj in mobjs.iterate() do
+			if mobj.type ~= MT_COIN then continue end
+			table.insert(exchangetable, mobj)
+		end
+
+		for k,v in ipairs(exchangetable) do
+			P_RemoveMobj(v)
+		end
+	end
+
+	if PKZ_Table.hardMode then
+		local exchangetable = {}
+
+		for mobj in mobjs.iterate() do
+			if not hard_mode_replacements[mobj.type] then continue end
+			table.insert(exchangetable, mobj)
+		end
+
+		for k,v in ipairs(exchangetable) do
+			hard_mode_replacements[v.type](v)
+		end
+
+		for s in sectors.iterate do
+			if hard_mode_flats[s.ceilingpic] then
+				s.ceilingpic = hard_mode_flats[s.ceilingpic]
+			end
+			if hard_mode_flats[s.floorpic] then
+				s.floorpic = hard_mode_flats[s.floorpic]
+			end
+		end
+
+		for k,v in ipairs(hard_mode_textur) do
+			print(k+" "+v)
+		end
+
+		for side in sides.iterate do
+			if hard_mode_textur[side.toptexture] ~= nil then
+				side.toptexture = hard_mode_textur[side.toptexture]
+			end
+			if hard_mode_textur[side.midtexture] ~= nil then
+				side.midtexture = hard_mode_textur[side.midtexture]
+			end
+			if hard_mode_textur[side.bottomtexture] ~= nil then
+				side.bottomtexture = hard_mode_textur[side.bottomtexture]
+			end
+		end
+
+
+
+
+addHook("PlayerSpawn", function(p)
+	if gamemap == 1 then
+		CONS_Printf(p, "To get into PKZ, press 'H' to access it.")
+	end
+	if not PKZ_Table.evil then return end
+	p.lives = 1
+	p.rings = 0
+end)
+
+addHook("MobjDeath", function(a)
+	if not PKZ_Table.evil then return end
+	if not (a.player and a.player.valid) then return end
+	a.player.lives = 0
+	a.player.rings = 0
+end, MT_PLAYER)
